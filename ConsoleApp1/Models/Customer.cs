e4c605d1a14d29489252f8c25ba51ad70989a2c5
Lin <163410806+usernamecccc@users.noreply.github.com>
2025-07-23 22:11:05 +0800

    Update Customer.cs

--- ConsoleApp1/Models/Customer.cs
+++ ConsoleApp1/Models/Customer.cs
@@ -9,6 +9,8 @@ namespace ConsoleApp1.Models
         public string Name { get; set; }
         public string PhoneNum { get; set; }
         public int VipLevel { get; set; }
+        public string Password_Hash { get; set; }
+        public string Salt { get; set; }
 
         // 默认构造函数
         public Customer()
@@ -16,12 +18,14 @@ namespace ConsoleApp1.Models
         }
 
         // 带参数的构造函数
-        public Customer(string customerID, string name, string phoneNum, int vipLevel)
+        public Customer(string customerID, string name, string phoneNum, int vipLevel, string password_hash, string salt  )
         {
             CustomerID = customerID;
             Name = name;
             PhoneNum = phoneNum;
             VipLevel = vipLevel;
+            Password_Hash=password_hash;
+            Salt=salt;
         }
 
         // 打印顾客信息的方法
@@ -31,6 +35,8 @@ namespace ConsoleApp1.Models
             Console.WriteLine($"顾客姓名: {Name}");
             Console.WriteLine($"联系电话: {PhoneNum}");
             Console.WriteLine($"会员等级: {VipLevel}");
+            Console.WriteLine($"登录密码: {Password_Hash}");
+            Console.WriteLine($"随机字符串: {Salt}");
         }
     }
 }
